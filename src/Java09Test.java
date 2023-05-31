df7a1f850b795b1f77b6edb89655058b0d06b4fa
cpufilecode <zf0908@naver.com>
2023-05-31 12:50:04 +0900

    Changes

--- src/Java09Test.java
+++ src/Java09Test.java
@@ -2,6 +2,8 @@ public class Java09Test {
     public static void main(String[] args) {
         int a = 5, b = 9, c;
         c = b % 5 < 5 ? 1 : 0;
+        c = c | c << 3;
+        c = a < 5 || c >= 10 ? c - a : c + a;
 
     }
 }
