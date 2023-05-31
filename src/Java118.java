588ac7ca8e34442de43bd43b004644b58bc56e5c
cpufilecode <zf0908@naver.com>
2023-05-31 12:18:49 +0900

    Changes

--- src/Java118.java
+++ src/Java118.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Java01 {
+public class Java118 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int a = scan.nextInt();
