cf2bf5025802c462f8cd92b3ac290a46a3ef69f9
cpufilecode <zf0908@naver.com>
2023-05-31 12:36:21 +0900

    Changes

--- src/Java08Test.java
+++ src/Java08Test.java
@@ -1,16 +1,16 @@
 public class Java08Test {
     public static void main(String[] args) {
-        int w = 3, x=4, y=3, z=5;
-        if((w==2|y==2) & !(y>z) & (1== x ^ y !=z)) {
-            w=x+y;
-            if(7==x ^ y !=w)
+        int w = 3, x = 4, y = 3, z = 5;
+        if((w == 2 | w == y) & !(y > z) & (1 == x ^ y != z)) {
+            w = x + y;
+            if(7 == x ^ y != w)
                 System.out.println(w);
             else
                 System.out.println(x);
         }
         else {
-            w=y+z;
-            if(7 == y ^ z !=w)
+            w = y + z;
+            if(7 == y ^ z != w)
                 System.out.println(w);
             else
                 System.out.println(z);
