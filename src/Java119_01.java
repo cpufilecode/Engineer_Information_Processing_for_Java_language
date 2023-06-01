ff55c7df522d0bf1546433286294d623ed47fbf2
cpufilecode <zf0908@naver.com>
2023-06-01 21:42:43 +0900

    Changes

--- src/Java119_01.java
+++ src/Java119_01.java
@@ -3,9 +3,11 @@ public class Java119_01 {
         int[] a = new int[8];
         int i = 0;
         int n = 10;
-        while(n>0) {
+        while(n > 0) {
             a[i++] = n % 2;
             n /= 2;
         }
+        for(i = 7; i >= 0; i--)
+            System.out.print(a[i]);
     }
 }
