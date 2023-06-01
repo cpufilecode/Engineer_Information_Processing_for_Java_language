2f14109074aa4ef19c7375c16923e6d0862e841e
cpufilecode <zf0908@naver.com>
2023-06-01 22:03:11 +0900

    Changes

--- src/Java119_02.java
+++ src/Java119_02.java
@@ -1,5 +1,9 @@
 public class Java119_02 {
     public static void main(String[] args) {
+        int ary[][] = new int[3][5];
+        int n = 1;
+        for (int i = 0; i < 3; i++) {
 
+        }
     }
 }
