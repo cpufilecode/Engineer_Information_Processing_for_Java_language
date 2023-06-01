2b2d45f6b6b07d6458e5f0a71b8bbdb31d978f92
cpufilecode <zf0908@naver.com>
2023-06-01 22:17:15 +0900

    Changes

--- src/Java119_03.java
+++ src/Java119_03.java
@@ -1,5 +1,10 @@
 public class Java119_03 {
     public static void main(String[] args) {
-
+        int i = 0, c = 0;
+        while (i < 10) {
+            i++;
+            c *= i;
+        }
+        System.out.println(c);
     }
 }
