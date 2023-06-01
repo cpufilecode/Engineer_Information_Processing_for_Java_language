562a324fd947dfced69fe9a9e799d257f3a5d63a
cpufilecode <zf0908@naver.com>
2023-06-01 21:31:45 +0900

    Changes

--- src/Java118_08.java
+++ src/Java118_08.java
@@ -1,4 +1,4 @@
-public class Java08Test {
+public class Java118_08 {
     public static void main(String[] args) {
         int w = 3, x = 4, y = 3, z = 5;
         if((w == 2 | w == y) & !(y > z) & (1 == x ^ y != z)) {
