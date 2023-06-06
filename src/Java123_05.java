b38577c024a69a9d16d0a8c2a5b2097663c41f8f
cpufilecode <zf0908@naver.com>
2023-06-07 00:11:21 +0900

    Changes

--- src/Java123_05.java
+++ src/Java123_05.java
@@ -1,2 +1,20 @@
+class a {
+    int a;
+    int b;
+}
 public class Java123_05 {
+    static void func1(a m) {
+        m.a *= 10;
+    }
+    static void func2(a m) {
+        m.a += m.b;
+    }
+    public static void main(String args[]) {
+        a m = new a();
+        m.a = 100;
+        func1(m);
+        m.b = m.a;
+        func2(m);
+        System.out.printf("%d", m.a);
+    }
 }
