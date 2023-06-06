17adcb9867973c3e6a82921d659915b5a0f9babb
cpufilecode <zf0908@naver.com>
2023-06-06 23:47:01 +0900

    Changes

--- src/Java123_04.java
+++ src/Java123_04.java
@@ -1,2 +1,16 @@
 public class Java123_04 {
+
+}
+
+class cond {
+    int a;
+    public cond(int a) {
+        this.a = a;
+    }
+    public int func( ) {
+        int b = 1;
+        for (int i = 1; i < a; i++)
+            b += a * i;
+        return a + b;
+    }
 }
