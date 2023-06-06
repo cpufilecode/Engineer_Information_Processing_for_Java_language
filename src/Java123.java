9aaa02497e5b04b9ffdc78b98e97f2b9cdb2050a
cpufilecode <zf0908@naver.com>
2023-06-06 14:55:00 +0900

    Changes

--- src/Java123.java
+++ src/Java123.java
@@ -1,5 +1,11 @@
+class ClassA {
+
+}
 public class Java123 {
     public static void main(String[] args) {
-
+        int x = 3, y = 6, r;
+        ClassA cal = new ClassA();
+        r = cal.funcAdd(x, y);
+        System.out.print(r);
     }
 }
