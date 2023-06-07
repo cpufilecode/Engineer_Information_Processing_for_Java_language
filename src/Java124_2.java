67295830cc68493ad84b21397f357049bd1cfaab
cpufilecode <zf0908@naver.com>
2023-06-07 14:38:37 +0900

    Changes

--- src/Java124_2.java
+++ src/Java124_2.java
@@ -1,6 +1,9 @@
 abstract class Vehicle {
     String name;
     abstract public String getName(String val);
+    public String getName() {
+        return "Vehicle name : " + name;
+    }
 }
 class Car extends Vehicle {
 
